b364dc7590f29274c16ccf2ff6893887802aee92
Koehler, A (Arno) <arno.koehler@rabobank.nl>
2019-04-01 21:51:36 +0200

    Still to eager but now just letting it be as is.

--- src/test/java/nl/rabobank/c4/cleanexample/bankaccount/internal/BankAccountComponentIT.java
+++ src/test/java/nl/rabobank/c4/cleanexample/bankaccount/internal/BankAccountComponentIT.java
@@ -6,26 +6,35 @@ import nl.rabobank.c4.cleanexample.CleanExampleApplication;
 import nl.rabobank.c4.cleanexample.bankaccount.BankAccountComponent;
 import nl.rabobank.c4.cleanexample.bankaccount.domain.BankAccount;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-@WebAppConfiguration
 @RunWith(SpringRunner.class)
-@ActiveProfiles("local")
-@SpringBootTest(classes = { CleanExampleApplication.class, BankaccountRepository.class, BankAccountEntityMapper.class, BankAccountComponentDefault.class })
+@SpringBootTest(classes = CleanExampleApplication.class)
 public class BankAccountComponentIT {
 
     @Autowired
     private BankAccountComponent bankAccountComponent;
 
+    @Autowired
+    private BankaccountRepository bankaccountRepository;
+
+    @Before
+    public void before() {
+        BankAccountEntity bankAccount = new BankAccountEntity();
+        bankAccount.setBankAccountNumber("123");
+        bankAccount.setLastName("jansen");
+        bankAccount.setInitals("tst");
+        bankaccountRepository.save(bankAccount);
+    }
+
     @Test
     public void shouldGetBankAccount() {
-        BankAccount result = bankAccountComponent.getBankaccount("1234567");
+        BankAccount result = bankAccountComponent.getBankaccount("123");
 
         assertThat(result).isNotNull();
     }
